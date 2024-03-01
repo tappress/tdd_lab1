8b63b6b2d89ab64a502a40982e946fb2641ddd07
Andrii <tap.press.swap@gmail.com>
2024-03-01 23:25:29 +0200

    rename function

--- src/converter.py
+++ src/converter.py
@@ -2,7 +2,7 @@ VAL = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
 SYB = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
 
 
-def int_to_roman(num: int) -> str:
+def decimal_to_roman(num: int) -> str:
     if num <= 0:
         raise ValueError("Input must be a positive integer")
 
--- tests/test_decimal_to_roman.py
+++ tests/test_decimal_to_roman.py
@@ -1,11 +1,11 @@
 import pytest
 
-from converter import int_to_roman
+from converter import decimal_to_roman
 
 
 @pytest.mark.parametrize("input_value,expected_output", [(1, "I"), (5, "V"), (10, "X")])
 def test_standard_numbers(input_value, expected_output):
-    assert int_to_roman(input_value) == expected_output
+    assert decimal_to_roman(input_value) == expected_output
 
 
 @pytest.mark.parametrize(
@@ -13,30 +13,30 @@ def test_standard_numbers(input_value, expected_output):
     [(4, "IV"), (9, "IX"), (40, "XL"), (90, "XC"), (400, "CD"), (900, "CM")],
 )
 def test_subtractive_notation(input_value, expected_output):
-    assert int_to_roman(input_value) == expected_output
+    assert decimal_to_roman(input_value) == expected_output
 
 
 @pytest.mark.parametrize(
     "input_value,expected_output", [(58, "LVIII"), (1994, "MCMXCIV")]
 )
 def test_complex_numbers(input_value, expected_output):
-    assert int_to_roman(input_value) == expected_output
+    assert decimal_to_roman(input_value) == expected_output
 
 
 @pytest.mark.parametrize("input_value,expected_output", [(1, "I")])
 def test_lower_boundary(input_value, expected_output):
-    assert int_to_roman(input_value) == expected_output
+    assert decimal_to_roman(input_value) == expected_output
 
 
 @pytest.mark.parametrize("input_value,expected_output", [(3888, "MMMDCCCLXXXVIII")])
 def test_larger_number(input_value, expected_output):
-    assert int_to_roman(input_value) == expected_output
+    assert decimal_to_roman(input_value) == expected_output
 
 
 @pytest.mark.parametrize("input_value", [0, -1])
 def test_zero_or_negative_input(input_value):
     with pytest.raises(ValueError):
-        int_to_roman(input_value)
+        decimal_to_roman(input_value)
 
 
 @pytest.mark.parametrize(
@@ -53,4 +53,4 @@ def test_zero_or_negative_input(input_value):
     ],
 )
 def test_varied_numbers(input_value, expected_output):
-    assert int_to_roman(input_value) == expected_output
+    assert decimal_to_roman(input_value) == expected_output
